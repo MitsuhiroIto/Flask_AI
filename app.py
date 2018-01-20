c78366293e9276e61983bc0c4c83b152066fa9e0
mitsuhiro <mitsuhiro.ito@furukawaelectric.com>
2018-01-21 02:52:13 +0900

    背景調整

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ import os
 import sqlite3
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
 from werkzeug import secure_filename
-import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 app = Flask(__name__)
 
-UPLOAD_FOLDER = './static'
+UPLOAD_FOLDER = './static/uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif','jpeg'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -19,6 +18,10 @@ def allowed_file(filename):
 def index():
     return render_template('index.html')
 
+@app.route('/augmentation_initial')
+def augmentation_initial():
+    return render_template('augmentation.html')
+
 @app.route('/send', methods=['GET', 'POST'])
 def send():
     if request.method == 'POST':
@@ -26,22 +29,22 @@ def send():
         if img_file and allowed_file(img_file.filename):
             img_name = secure_filename(img_file.filename)
             img_file.save(os.path.join(app.config['UPLOAD_FOLDER'], img_name))
-            #print(os.path.join(app.config['UPLOAD_FOLDER'], img_name))
-            return render_template('index.html', img_name=img_name)
+            return render_template('augmentation.html', img_name=img_name)
         else:
-            return ''' <p>許可されていない拡張子です</p> '''
+            return render_template('augmentation.html', warning="warning")
     else:
-        return redirect(url_for('index'))
+        return redirect(url_for('augmentation'))
 
-@app.route('/augumentation', methods = ['POST'])
-def augumentation():
-    print(url_for('static', filename=request.form['image']))
-    im = cv2.imread("." + url_for('static', filename=request.form['image']))
+
+@app.route('/augmentation', methods = ['POST'])
+def augmentation():
+    print(url_for('static', filename= "uploads/" + request.form['image']))
+    im = cv2.imread("." + url_for('static', filename= "uploads/" + request.form['image']))
     im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     im_edge = cv2.Canny(im_gray,100,200)
-    img_arg_url= "." + url_for('static', filename=request.form['image']).rsplit('.', 1)[0] + "_edge." + request.form['image'].rsplit('.', 1)[1]
+    img_arg_url= "." + url_for('static', filename="uploads/" + request.form['image']).rsplit('.', 1)[0] + "_edge." + request.form['image'].rsplit('.', 1)[1]
     cv2.imwrite(img_arg_url,im_edge)
-    return render_template('augumentation.html', img_arg_url=img_arg_url)
+    return render_template('augmentation.html', img_arg_url=img_arg_url)
 
 
 if __name__ == '__main__':
