53bbd5a18096aa46333f7b0d778c0ceaac1593ae
mitsuhiro <mitsuhiro.ito@furukawaelectric.com>
2018-01-22 23:06:24 +0900

    data_augmentation Samples部分背景　修正

--- application.py
+++ application.py
@@ -16,7 +16,6 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-
 ############### 初めのページ#############################
 @app.route('/')
 def index():
@@ -40,15 +39,58 @@ def augmentation_send():
     else:
         return redirect(url_for('augmentation'))
 
-
 @app.route('/augmentation_augmentation', methods = ['POST'])
 def augmentation_augmentation():
-    print(url_for('static', filename= "uploads/" + request.form['image']))
+    #print(url_for('static', filename= "uploads/" + request.form['image']))
     im = cv2.imread("." + url_for('static', filename= "uploads/" + request.form['image']))
-    im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    im_edge = cv2.Canny(im_gray,100,200)
-    img_arg_url= "." + url_for('static', filename="uploads/" + request.form['image']).rsplit('.', 1)[0] + "_edge." + request.form['image'].rsplit('.', 1)[1]
-    cv2.imwrite(img_arg_url,im_edge)
+
+    if request.form['button_name'] == "Gray":
+        im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        img_arg_url= "." + url_for('static', filename="uploads/" + request.form['image']).rsplit('.', 1)[0] + "_gray." + request.form['image'].rsplit('.', 1)[1]
+        cv2.imwrite(img_arg_url,im_gray)
+        return render_template('augmentation.html', img_arg_url=img_arg_url)
+
+    if request.form['button_name'] == "Sobel_y":
+        im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        img_arg_url= "." + url_for('static', filename="uploads/" + request.form['image']).rsplit('.', 1)[0] + "_sobely." + request.form['image'].rsplit('.', 1)[1]
+        im_sobely = cv2.Sobel(im_gray, cv2.CV_32F, 0, 1, ksize=3)
+        cv2.imwrite(img_arg_url,im_sobely)
+        return render_template('augmentation.html', img_arg_url=img_arg_url)
+
+    if request.form['button_name'] == "Sobel_x":
+        im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        img_arg_url= "." + url_for('static', filename="uploads/" + request.form['image']).rsplit('.', 1)[0] + "_sobelx." + request.form['image'].rsplit('.', 1)[1]
+        im_sobelx = cv2.Sobel(im_gray, cv2.CV_32F, 1, 0, ksize=3)
+        cv2.imwrite(img_arg_url,im_sobelx)
+        return render_template('augmentation.html', img_arg_url=img_arg_url)
+
+    if request.form['button_name'] == "Canny":
+        im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        im_canny = cv2.Canny(im_gray,100,200)
+        img_arg_url= "." + url_for('static', filename="uploads/" + request.form['image']).rsplit('.', 1)[0] + "_canny." + request.form['image'].rsplit('.', 1)[1]
+        cv2.imwrite(img_arg_url,im_canny)
+
+    if request.form['button_name'] == "Rotation":
+        angle = 20
+        M = cv2.getRotationMatrix2D((int(im.shape[0]/2),int(im.shape[1]/2)),angle,1)
+        im_rotation = cv2.warpAffine(im,M,(im.shape[1], im.shape[0]))
+        img_arg_url= "." + url_for('static', filename="uploads/" + request.form['image']).rsplit('.', 1)[0] + "_rotation." + request.form['image'].rsplit('.', 1)[1]
+        cv2.imwrite(img_arg_url,im_rotation)
+
+    if request.form['button_name'] == "Flip":
+        im_flip = np.fliplr(im)
+        img_arg_url= "." + url_for('static', filename="uploads/" + request.form['image']).rsplit('.', 1)[0] + "_flip." + request.form['image'].rsplit('.', 1)[1]
+        cv2.imwrite(img_arg_url,im_flip)
+
+    if request.form['button_name'] == "Noise":
+        s_vs_p = 0.5
+        amount = 0.004
+        im_noise = im.copy()
+        num_pepper = np.ceil(amount* im_noise.size * (1. - s_vs_p))
+        coords = [np.random.randint(0, i-1 , int(num_pepper)) for i in im_noise.shape]
+        im_noise[coords[:-1]] = (0,0,0)
+        img_arg_url= "." + url_for('static', filename="uploads/" + request.form['image']).rsplit('.', 1)[0] + "_noise." + request.form['image'].rsplit('.', 1)[1]
+        cv2.imwrite(img_arg_url,im_noise)
     return render_template('augmentation.html', img_arg_url=img_arg_url)
 
 
@@ -99,7 +141,6 @@ def mask_rcnn_detectio():
     image_url = url_for('static', filename= "uploads/" + request.form['image'])
     mask_rcnn_url = mask_rcnn_detect(image_url)
     return render_template('mask_rcnn.html', mask_rcnn_url=mask_rcnn_url)
-
 
 
 if __name__ == '__main__':
